faecb893e2bf1520a7c047a7339dd7f23270dbf1
JDgomez <87883400+JDgomez2002@users.noreply.github.com>
2023-06-01 22:29:50 -0600

    ESlint final config

--- proy1-web/src/App.jsx
+++ proy1-web/src/App.jsx
@@ -1,8 +1,37 @@
 import './App.css'
 
+let arr = [
+  1,
+  2,
+  3,
+  4,
+]
+
 function App() {
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <div>asd</div>
+  return (
+    <div className="">
+      asd
+      <div>hello</div>
+    </div>
+  )
 }
 
 export default App
+
+/*
+[Criterio subjetivo] (40 puntos) Según qué tan similar sea su diseño al sitio en el que se basaron,
+  deben enviar el sitio en los comentarios.
+(10 puntos) Si entregan un link a su proyecto publicado en el servidor de la clase
+(20 puntos) Por explicar a través de un README bien formateado (hint) qué tecnologías implementaron
+(5 puntos) Por implementar React
+(5 puntos) Por implementar css modules
+15 puntos) Por implementar ESLint con las reglas:
+    'plugin:react/recommended',
+    'airbnb-base',
+    'airbnb/rules/react',
+Su código no puede tener errores para que cuente
+Deben proveer un script "lint"
+(10 puntos) Por implementar al menos un test visual sobre su código usando storybook
+
+(50 puntos) Por implementar Server Side Rendering en su aplicación (sin utilizar ninguna librería adicional) (hint)
+*/
